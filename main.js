efdf0022841a41d7c15fb30ba6c5a432118a2218
Shrikrishnaprasad <sgkrishna619@gmail.com>
2021-08-11 19:19:18 +0530

    main.js ignore

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-const num = process.argv[2];
-const arr = JSON.parse(num);
-const maxNum = Math.max(...arr);
+import os from "os";
+import path from "path";
+// const num = process.argv[2];
+// const arr = JSON.parse(num);
+// const maxNum = Math.max(...arr);
 
-console.log("The max num is: " + maxNum);
+// console.log("The max num is: " + maxNum);
+
+console.log(os.totalmem(), os.freemem());
+console.log(path.parse(__filename));
